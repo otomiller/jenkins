1d1ea1c6cec9fc9df26bab3586cd751b20a22888
Oto Miller <oto@synectics.dev>
2023-03-06 10:11:53 -0500

    Update ecr_tags_list.groovy

--- ecr_tags_list.groovy
+++ ecr_tags_list.groovy
@@ -59,47 +59,6 @@ ecr_images.sort{ a, b ->
 ecr_images.push("NA")
 return ecr_images.reverse()
                 '''
-            ]
-            ]
-        ],
-
-        [$class: 'ChoiceParameter', 
-        choiceType: 'PT_SINGLE_SELECT', 
-        description: '', 
-        filterable: false, 
-        name: 'dots_analytics_intake_image_version', 
-        randomName: 'choice-parameter-21337077649521571', 
-            script: [
-                $class: 'GroovyScript', 
-                fallbackScript: [
-                    classpath: [], 
-                    sandbox: false, 
-                    script: 'return ["ERROR"]'], 
-            script: [
-                classpath: [], 
-                sandbox: false, 
-                script: '''
-import groovy.json.JsonSlurper
-def ecr_images_json = ['bash', '-c', "aws ecr list-images --repository-name dots-microservices --filter tagStatus=TAGGED --region us-east-1"].execute().text
-def data = new JsonSlurper().parseText(ecr_images_json)
-def ecr_images = []; 
-data.imageIds.each {
-    if ( ( "$it.imageTag".length() >= 1 ) && ( "$it.imageTag" != "latest" ) && ( !"$it.imageTag".contains("-pr-") ) ) {
-        ecr_images.push("$it.imageTag")
-        }
-}
-ecr_images.sort{ a, b -> 
-    def aList = a.findAll(/\\d+/)
-    def bList = b.findAll(/\\d+/)
-    for ( int i = 0 ; i < aList.size() ; i++ ) {
-        def aVal = aList[i] ? aList[i].toInteger() : 0
-        def bVal = bList[i] ? bList[i].toInteger() : 0
-        if ( aVal <=> bVal ) { return aVal <=> bVal }    }
-    bList.size() > aList.size() ? -1 : 0 }
-ecr_images.push("NA")
-return ecr_images.reverse()
-                '''
-            ]
-            ]
+                ]
         ]) // parameters
 ]) // properties
